03bf73943443a6eebb37d3d767bc79f22eae5be4
xiaoxiong917 <115796483+xiaoxiong917@users.noreply.github.com>
2023-05-21 17:03:09 +0800

    Add files via upload

--- vit.py
+++ vit.py
@@ -11,14 +11,15 @@ class ViT(nn.Module):
 
         self.patch = patch # number of patches in one row(or col)
         self.is_cls_token = is_cls_token
-        self.patch_size = img_size//self.patch
-        f = (img_size//self.patch)**2*3 # 48 # patch vec length
-        num_tokens = (self.patch**2)+1 if self.is_cls_token else (self.patch**2)
+        self.patch_size = img_size//self.patch #32/8=4
+        f = (img_size//self.patch)**2*3 # 48 # patch vec length 每个块的向量长度[4, 4, 3] -> [48]
+        # [32, 32, 3] -> [4, 4, 3, 8]  8x8=64个token
+        num_tokens = (self.patch**2)+1 if self.is_cls_token else (self.patch**2)# 记录一张图片的总token数 8x8+1=65个 token， 用于添加训练的 token 位置编码
 
-        self.emb = nn.Linear(f, hidden) # (b, n, f)
-        self.cls_token = nn.Parameter(torch.randn(1, 1, hidden)) if is_cls_token else None
-        self.pos_emb = nn.Parameter(torch.randn(1,num_tokens, hidden))
-        enc_list = [TransformerEncoder(hidden,mlp_hidden=mlp_hidden, dropout=dropout, head=head) for _ in range(num_layers)]
+        self.emb = nn.Linear(f, hidden) # (b, n, f)#全连接层 [48] -> [384]
+        self.cls_token = nn.Parameter(torch.randn(1, 1, hidden)) if is_cls_token else None #加一个token
+        self.pos_emb = nn.Parameter(torch.randn(1,num_tokens, hidden))#位置 #加一组token
+        enc_list = [TransformerEncoder(hidden,mlp_hidden=mlp_hidden, dropout=dropout, head=head) for _ in range(num_layers)] #transformerencoder
         self.enc = nn.Sequential(*enc_list)
         self.fc = nn.Sequential(
             nn.LayerNorm(hidden),
@@ -27,34 +28,33 @@ class ViT(nn.Module):
 
 
     def forward(self, x):
-        out = self._to_words(x)
-        out = self.emb(out)
+        out = self._to_words(x)# [128, 3, 32, 32] -> [128, 64, 48]
+        out = self.emb(out) # Patch Embedding: [128, 64, 48] -> [128, 64, 384]
         if self.is_cls_token:
-            out = torch.cat([self.cls_token.repeat(out.size(0),1,1), out],dim=1)
-        out = out + self.pos_emb
-        out = self.enc(out)
+            out = torch.cat([self.cls_token.repeat(out.size(0),1,1), out],dim=1)# 插入 Position Embedding
+        out = out + self.pos_emb# Add Class token: [128, 64, 384] -> [128, 65, 384]
+        out = self.enc(out)# Transform encoding  [128, 65, 384] -> [128, 65, 384]
         if self.is_cls_token:
             out = out[:,0]
         else:
             out = out.mean(1)
-        out = self.fc(out)
+        out = self.fc(out)# [128, 65, 384] -> [128, 10]
         return out
 
-    def _to_words(self, x):
+    def _to_words(self, x):%将图片信息转换格式
         """
         (b, c, h, w) -> (b, n, f)
         """
-        out = x.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size).permute(0,2,3,4,5,1)
+        out = x.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size).permute(0,2,3,4,5,1)#permute维度互换
         out = out.reshape(x.size(0), self.patch**2 ,-1)
         return out
 
-
 if __name__ == "__main__":
-    b,c,h,w = 4, 3, 32, 32
+    b, c, h, w = 4, 3, 32, 32
     x = torch.randn(b, c, h, w)
-    net = ViT(in_c=c, num_classes= 10, img_size=h, patch=16, dropout=0.1, num_layers=7, hidden=384, head=12, mlp_hidden=384, is_cls_token=False)
+    net = ViT(in_c=c, num_classes=10, img_size=h, patch=16, dropout=0.1, num_layers=7, hidden=384, head=12,
+              mlp_hidden=384, is_cls_token=False)
     # out = net(x)
     # out.mean().backward()
-    torchsummary.summary(net, (c,h,w))
+    torchsummary.summary(net, (c, h, w))
     # print(out.shape)
-    
